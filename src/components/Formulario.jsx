74818723ffb09f71b3aa459c094e4b7b515ed714
Rodrigo Medina P <rmedina@mountain-creative.com>
2024-04-27 20:16:55 -0400

    Se agrego regex para formato de password

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,99 +1,94 @@
-
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import PropTypes from 'prop-types';
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import PropTypes from "prop-types";
 
 export const Formulario = ({ onChange, handleErrors }) => {
-	const onSubmit = (event) => {
-		event.preventDefault();
-		console.log(event.target)
-		const emailRegex = /^[A-Za-z0-9._%+-]+@[a-z._-]+\.[A-Za-z]{2,}$/;
-		const { nombre, correo, password, passwordRepeat } = event.target;
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(event.target);
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/; // 8-15 caracteres, una mayúscula, una minúscula, un número y un carácter especial
+    const { nombre, correo, password, passwordRepeat } = event.target;
 
-		if (
-			!nombre.value ||
-			!correo.value ||
-			!password.value ||
-			!passwordRepeat.value
-		) {
-			handleErrors('Todos los campos obligatorios');
-			return;
-		}
+    if (
+      !nombre.value ||
+      !correo.value ||
+      !password.value ||
+      !passwordRepeat.value
+    ) {
+      handleErrors("Todos los campos obligatorios");
+      return;
+    }
 
-		if (!emailRegex.test(correo.value)) {
-			handleErrors('Correo Incorrecto');
-			return;
-		}
+    if (!emailRegex.test(correo.value)) {
+      handleErrors(
+        "Verifica que el correo sea válido y no contenga espacios en blanco"
+      );
+      return;
+    }
 
-		if (password.value != passwordRepeat.value) {
-			handleErrors('Las contraseñas no coinciden');
-			return;
-		}
+    if (!passwordRegex.test(password.value)) {
+      handleErrors(
+        "La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula, un número y un carácter especial"
+      );
+      return;
+    }
 
-		handleErrors('Registro exitoso');
-	};
+    if (password.value != passwordRepeat.value) {
+      handleErrors("Las contraseñas no coinciden");
+      return;
+    }
 
-	return (
-		<Form onSubmit={onSubmit}>
-			<Form.Group
-				className="mb-3"
-				controlId="nombre"
-			>
-				<Form.Control
-					name="nombre"
-					type="text"
-					placeholder="Nombre"
-					onChange={onChange}
-				/>
-			</Form.Group>
+    handleErrors("Registro exitoso");
+  };
 
-			<Form.Group
-				className="mb-3"
-				controlId="correo"
-			>
-				<Form.Control
-					name="correo"
-					type="email"
-					placeholder="Correo"
-					onChange={onChange}
-				/>
-			</Form.Group>
+  return (
+    <Form onSubmit={onSubmit}>
+      <Form.Group className="mb-3" controlId="nombre">
+        <Form.Control
+          name="nombre"
+          type="text"
+          placeholder="Nombre"
+          onChange={onChange}
+        />
+      </Form.Group>
 
-			<Form.Group
-				className="mb-3"
-				controlId="password"
-			>
-				<Form.Control
-					name="password"
-					type="password"
-					placeholder="Contraseña"
-					onChange={onChange}
-				/>
-			</Form.Group>
+      <Form.Group className="mb-3" controlId="correo">
+        <Form.Control
+          name="correo"
+          type="email"
+          placeholder="Correo"
+          onChange={onChange}
+        />
+      </Form.Group>
 
-			<Form.Group
-				className="mb-3"
-				controlId="password2"
-			>
-				<Form.Control
-					name="passwordRepeat"
-					type="password"
-					placeholder="Verifica contraseña"
-					onChange={onChange}
-				/>
-			</Form.Group>
+      <Form.Group className="mb-3" controlId="password">
+        <Form.Control
+          name="password"
+          type="password"
+          placeholder="Contraseña"
+          onChange={onChange}
+        />
+      </Form.Group>
 
-			<Button
-				variant="success"
-				type="submit"
-			>
-				Registrarse
-			</Button>
-		</Form>
-	);
+      <Form.Group className="mb-3" controlId="password2">
+        <Form.Control
+          name="passwordRepeat"
+          type="password"
+          placeholder="Verifica contraseña"
+          onChange={onChange}
+        />
+      </Form.Group>
+
+      <Button variant="success" type="submit">
+        Registrarse
+      </Button>
+    </Form>
+  );
 };
 
 Formulario.propTypes = {
-	onChange: PropTypes.func,
-	handleErrors: PropTypes.func,
+  onChange: PropTypes.func,
+  handleErrors: PropTypes.func,
 };
